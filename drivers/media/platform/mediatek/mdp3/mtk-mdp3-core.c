bf9d5a437f4a7bb09c36c8229e51042d91b5209e
Chun-Kuang Hu <chunkuang.hu@kernel.org>
2024-02-13 02:00:30 +0000

    media: platform: mtk-mdp3: drop calling cmdq_pkt_finalize()
    Because client driver has the information of struct cmdq_client, so
    it's not necessary to store struct cmdq_client in struct cmdq_pkt.
    cmdq_pkt_finalize() use struct cmdq_client in struct cmdq_pkt, so it's
    going to be abandoned. Therefore, use cmdq_pkt_eoc() and cmdq_pkt_nop()
    to replace cmdq_pkt_finalize().
    
    Signed-off-by: Chun-Kuang Hu <chunkuang.hu@kernel.org>

--- drivers/media/platform/mediatek/mdp3/mtk-mdp3-core.c
+++ drivers/media/platform/mediatek/mdp3/mtk-mdp3-core.c
@@ -231,6 +231,8 @@ static int mdp_probe(struct platform_device *pdev)
 		goto err_put_scp;
 	}
 
+	mdp->cmdq_shift_pa = cmdq_get_shift_pa(mdp->cmdq_clt->chan);
+
 	init_waitqueue_head(&mdp->callback_wq);
 	ida_init(&mdp->mdp_ida);
 	platform_set_drvdata(pdev, mdp);
